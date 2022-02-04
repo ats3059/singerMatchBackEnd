41cd0742c563554821dfd16c11b20776d8a4e4ba
ats3059 <ats3059@naver.com>
2022-02-04 20:35:17 +0900

    modfiy

--- src/main/java/success/singermatch/SingerMatchApplication.java
+++ src/main/java/success/singermatch/SingerMatchApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SingermatchApplication {
+public class SingerMatchApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SingermatchApplication.class, args);
+		SpringApplication.run(SingerMatchApplication.class, args);
 	}
 
 }
