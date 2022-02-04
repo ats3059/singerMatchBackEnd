2d135da78ceda828e9441b384045559cba3bf26f
ats3059 <ats3059@naver.com>
2022-02-04 20:29:44 +0900

    initial commit

---
+++ src/main/java/success/singermatch/SingermatchApplication.java
@@ -0,0 +1,13 @@
+package success.singermatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SingermatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SingermatchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/success/singermatch/SingermatchApplicationTests.java
@@ -0,0 +1,13 @@
+package success.singermatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SingermatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
