7ac72d76590ffdf97537ebfa5d70ece59610c596
jinsb1995 <jinsb1995@gmail.com>
2022-02-07 23:39:13 +0900

    add mailTemplate

--- src/main/java/success/singermatch/domain/tmp1/api/TmpController.java
+++ src/main/java/success/singermatch/domain/tmp1/api/TmpController.java
@@ -1,16 +1,51 @@
 package success.singermatch.domain.tmp1.api;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import success.singermatch.global.util.MailUtils;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @Slf4j
+@RequiredArgsConstructor
 public class TmpController {
+
+    private final MailUtils mailUtils;
 
     @GetMapping("/tmp")
     public String tmp() {
         log.info("여기는 임시 콘트롤러 입니다.");
+        return "OK";
+    }
+
+    @GetMapping("/mailTest")
+    public String mailTest(String name) {
+        log.info("메일 테스트");
+
+        // 메일에 넣어줄 값
+        Map<String, String> values = new HashMap<>();
+        values.put("name", "배진성");
+        // to는 들어온 값
+        mailUtils.send("가입을 환영합니다.", "jinsb1995@gmail.com", "welcome", values);
+
+        return "OK";
+    }
+    
+    @GetMapping("/matchTest")
+    public String matchTest() {
+        log.info("매칭 테스트");
+
+        // 메일에 넣어줄 값
+        Map<String, String> values = new HashMap<>();
+        values.put("from", "배진성");
+        values.put("to", "안태선");
+        // to는 들어온 값
+        mailUtils.send("좋아요 안내", "jinsb1995@gmail.com", "match", values);
+
         return "OK";
     }
 }
--- src/main/java/success/singermatch/global/util/MailUtils.java
+++ src/main/java/success/singermatch/global/util/MailUtils.java
@@ -5,6 +5,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMailMessage;
 import org.springframework.stereotype.Component;
+import org.thymeleaf.context.Context;
+import org.thymeleaf.spring5.SpringTemplateEngine;
 import success.singermatch.global.common.MailProperties;
 
 import javax.mail.Message;
@@ -14,6 +16,7 @@ import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeBodyPart;
 import javax.mail.internet.MimeMessage;
 import javax.mail.internet.MimeMultipart;
+import java.util.Map;
 
 @Slf4j
 @Component
@@ -24,43 +27,33 @@ public class MailUtils {
 
     private final JavaMailSender mailSender;
 
+    private final SpringTemplateEngine templateEngine;
+
     /**
      *  메일 발송
-     *
+     * @Param title 메일 제목
+     * @Param to 수신자
+     * @Param templateName 이메일 템플릿 명
+     * @Param values 이메일에 들어갈 값
      */
-    public boolean send() {
+    public void send(String title, String to, String templateName, Map<String, String> values) {
 
-        String to = "jinsb1995@gmail.com";
-
-        String sendFrom = mailProperties.getFromMail();
-
-        String subject = "제목입니다.";
-
-        StringBuilder content = new StringBuilder();
-        content.append("<html>");
-        content.append("<body>");
-        content.append("<p>Hello</p>");
-        content.append("<b>Hello</b>");
-        content.append("</body>");
-        content.append("</html>");
-
-        // 테스트만 하면 됨
+        String mailContent = createMailTemplate(templateName, values);
 
         try {
             MimeMessage mm = mailSender.createMimeMessage();
+            
             mm.setRecipient(Message.RecipientType.TO, setAddress(to));
-            mm.setFrom(setAddress(sendFrom));
-            mm.setSubject(subject);
+            mm.setFrom(setAddress(mailProperties.getFromMail()));
 
+            mm.setSubject(title);
             // 내용에 html을 담기 위한 작업
-            setMailContent(content.toString(), mm);
+            setMailContent(mailContent, mm);
 
             mailSender.send(mm);
         } catch(Exception e) {
             log.info(e.getMessage());
         }
-
-        return true;
     }
 
     // 내용에 html을 담기 위한 작업
@@ -74,12 +67,32 @@ public class MailUtils {
         mm.setContent(mPart);
     }
 
-    // 단건 - 수신, 발신자를 Address객체로 반환시켜준다.
+
+    // 메일 템플릿에 들어갈 값 세팅
+    private String createMailTemplate(String templateName, Map<String, String> values) {
+        Context context = new Context();
+        values.forEach((key, value) -> {
+            log.info("key = {}, value = {}", key, value);
+            context.setVariable(key, value);
+        });
+
+        return templateEngine.process(templateName, context);
+    }
+
+    /**
+     * 단건 - 수신, 발신자를 Address객체로 반환시켜준다.
+     * @Param mail 수신자 메일
+     * usage - mm.setRecipients(Message.RecipientType.TO, setAddress(to));
+     */
     private InternetAddress setAddress(String mail) throws Exception {
         return new InternetAddress(mail);
     }
 
-    // 다건 - 수신, 발신자를 Address객체로 반환시켜준다.
+    /**
+     * 다건 - 수신, 발신자를 Address객체로 반환시켜준다.
+     * @Param mails 수신자 메일 배열
+     * usage - mm.setRecipients(Message.RecipientType.TO, setAddresses(tos));
+     */
     private InternetAddress[] setAddresses(String[] mails) throws Exception {
         InternetAddress[] mailsArr = new InternetAddress[mails.length];
 
@@ -90,3 +103,14 @@ public class MailUtils {
     }
 
 }
+
+
+
+//        StringBuilder content = new StringBuilder();
+//        content.append("<html>");
+//        content.append("<body>");
+//        content.append("<p>안태선님이 배진성님의 게시물에 좋아요를 누르셨습니다!</p><br />");
+//        content.append("<b>http://localhost:9800</b>");
+//        content.append("</body>");
+//        content.append("</html>");
+//        return content.toString();
