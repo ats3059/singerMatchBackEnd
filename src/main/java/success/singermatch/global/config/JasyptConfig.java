b8a200d44238674568ab689fd4496cb2a60e512d
jinsb1995 <jinsb1995@gmail.com>
2022-03-04 00:03:22 +0900

    add comment for jasypt

--- src/main/java/success/singermatch/global/config/JasyptConfig.java
+++ src/main/java/success/singermatch/global/config/JasyptConfig.java
@@ -4,19 +4,24 @@ import lombok.extern.slf4j.Slf4j;
 import org.jasypt.encryption.StringEncryptor;
 import org.jasypt.encryption.pbe.PooledPBEStringEncryptor;
 import org.jasypt.encryption.pbe.config.SimpleStringPBEConfig;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+/**
+ * 프로퍼티 파일에 노출되는 정보(비밀번호, 주소 등..)를 암호화
+ */
 @Slf4j
 @Configuration
 public class JasyptConfig {
-/*  1.   ***.properties 파일로 관리하려면 이 방법
+/*
+    1.   ***.properties 이용
     @Value("${jasypt.encryptor.password}")
     private String encryptKey;
+
+    2. VM Option
+    System.getProperty("jasypt.encryptor.password");
  */
 
-//  2. VM Option을 이용하려면 이 방법
     private String encryptKey = System.getProperty("jasypt.encryptor.password");
 
     private final static String ALGORITHM = "PBEWithMD5AndDES";
@@ -26,17 +31,16 @@ public class JasyptConfig {
         PooledPBEStringEncryptor encryptor = new PooledPBEStringEncryptor();
         SimpleStringPBEConfig config = new SimpleStringPBEConfig();
 
-        log.info("비밀번호 암호화 할 때 사용한 Key = {}", encryptor);
-        config.setPassword(encryptKey);    // 실제 password에 대입할 key
+        config.setPassword(encryptKey);    // 실제 비밀번호를 암/복호화 할 때 사용하는 Key
 
         // start default setting
-        config.setAlgorithm(ALGORITHM);
-        config.setKeyObtentionIterations("1000");
-        config.setPoolSize("1");
-        config.setProviderName("SunJCE");
-        config.setSaltGeneratorClassName("org.jasypt.salt.RandomSaltGenerator");
-        config.setIvGeneratorClassName("org.jasypt.iv.RandomIvGenerator");
-        config.setStringOutputType("base64");
+        config.setAlgorithm(ALGORITHM);                 // 암/복호화 알고리즘
+        config.setKeyObtentionIterations("1000");       // 반복할 해싱 횟수
+        config.setPoolSize("1");                        // 인스턴스 pool
+        config.setProviderName("SunJCE");               // 제조사?
+        config.setSaltGeneratorClassName("org.jasypt.salt.RandomSaltGenerator");      // salt 생성 클래스
+        config.setIvGeneratorClassName("org.jasypt.iv.RandomIvGenerator");            //
+        config.setStringOutputType("base64");           // encoding 방식
         // end default setting
 
         encryptor.setConfig(config);
